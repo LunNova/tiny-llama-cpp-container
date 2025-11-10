a9a48ef97b1f06e9198246e8ec9344f260d011e8
Luna Nova <git@lunnova.dev>
2025-11-09 23:59:33 -0800

    flake: simplify

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/master";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -10,28 +10,25 @@
       system:
       let
         inherit (nixpkgs) lib;
-        nixpkgsForTargetISA =
-          isa:
-          import nixpkgs {
-            inherit system;
-            config.rocmSupport = true;
-            overlays = lib.optional (isa != null) (
-              final: prev: {
-                rocmPackages_6 = prev.rocmPackages_6.overrideScope (
-                  fs: ps: {
-                    clr = ps.clr.override {
-                      localGpuTargets = [ isa ];
-                    };
-                  }
-                );
-                rocmPackages = final.rocmPackages_6;
-              }
-            );
-          };
         legacyPackagesForTargetISA =
           isa:
           let
-            pkgs = nixpkgsForTargetISA isa;
+            pkgs = import nixpkgs {
+              inherit system;
+              config.rocmSupport = true;
+              overlays = lib.optional (isa != null) (
+                final: prev: {
+                  rocmPackages_6 = prev.rocmPackages_6.overrideScope (
+                    fs: ps: {
+                      clr = ps.clr.override {
+                        localGpuTargets = [ isa ];
+                      };
+                    }
+                  );
+                  rocmPackages = final.rocmPackages_6;
+                }
+              );
+            };
           in
           {
             inherit (pkgs) llama-cpp;
@@ -58,6 +55,8 @@
       {
         legacyPackages = (legacyPackagesForTargetISA null) // {
           gfx1030 = legacyPackagesForTargetISA "gfx1030";
+          gfx1100 = legacyPackagesForTargetISA "gfx1100";
+          gfx1200 = legacyPackagesForTargetISA "gfx1200";
         };
       }
     );
